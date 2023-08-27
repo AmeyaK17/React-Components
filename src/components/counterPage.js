d9aad6e206b3d3bb150f6f4268f42ec5053351ec
Ameya Kale <ameya.k.kale@gmail.com>
2023-08-26 23:57:41 -0400

    Tables

--- src/components/counterPage.js
+++ src/components/counterPage.js
@@ -8,6 +8,7 @@ const DECREMENT_COUNT = 'decrement'
 const SET_VALUE_TO_ADD = 'change_value_to_add'
 const ADD_VALUE_TO_COUNT = 'add_value_to_count'
 
+
 const reducer = (state, action) => {
     switch(action.type){
         case INCREMENT_COUNT:
